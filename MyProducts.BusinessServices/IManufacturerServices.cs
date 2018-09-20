1220c223532d732b6dc22edefb55ed70802ce7f6
Stefan Ivovic <stefanivovic91@gmail.com>
2018-09-20 20:03:17 +0200

    added manufacturer services

---
+++ MyProducts.BusinessServices/IManufacturerServices.cs
@@ -0,0 +1,13 @@
+﻿using MyProducts.BusinessEntities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProducts.BusinessServices
+{
+    public interface IManufacturerServices
+    {
+        void CreateManufacturer(ManufacturerEntity manufacturerEntity);
+    }
+}
---
+++ MyProducts.BusinessServices/ManufecturerServices.cs
@@ -0,0 +1,23 @@
+﻿using MyProducts.BusinessEntities;
+using MyProducts.DAL;
+using MyProducts.DAL.UnitOfWork;
+using System;
+
+namespace MyProducts.BusinessServices
+{
+    public class ManufecturerServices : IManufacturerServices
+    {
+        private UnitOfWork _unitOfWork;
+
+        public ManufecturerServices(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void CreateManufacturer(ManufacturerEntity manufacturerEntity)
+        {
+            var manufacturerDb = AutoMapper.Mapper.Map<ManufacturerEntity, Manufacturer>(manufacturerEntity);
+            _unitOfWork.ManufacturerRepository.Insert(manufacturerDb);
+            _unitOfWork.Save();
+        }
+    }
+}
