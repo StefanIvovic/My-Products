3eea98be735f956ead5c3d2e63beafd385c5493a
Stefan Ivovic <stefanivovic91@gmail.com>
2018-09-20 20:29:41 +0200

    added product services

---
+++ MyProducts.BusinessServices/IProductServices.cs
@@ -0,0 +1,14 @@
+﻿using MyProducts.BusinessEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProducts.BusinessServices
+{
+    public interface IProductServices
+    {
+        void CreateProduct(ProductEntity productEntity);
+    }
+}
---
+++ MyProducts.BusinessServices/ProductServices.cs
@@ -0,0 +1,22 @@
+﻿using MyProducts.BusinessEntities;
+using MyProducts.DAL;
+using MyProducts.DAL.UnitOfWork;
+
+namespace MyProducts.BusinessServices
+{
+    public class ProductServices : IProductServices
+    {
+        private UnitOfWork _unitOfWork;
+
+        public ProductServices(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void CreateProduct(ProductEntity productEntity)
+        {
+            var productDb = AutoMapper.Mapper.Map<ProductEntity, Product>(productEntity);
+            _unitOfWork.ProductRepository.Insert(productDb);
+            _unitOfWork.Save();
+        }
+    }
+}
