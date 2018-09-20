39d0cc9f8315274a50d3771895bf8f949e6542ba
Stefan Ivovic <stefanivovic91@gmail.com>
2018-09-20 19:24:35 +0200

    finished businessentity library

---
+++ MyProducts.BusinessEntities/CategoryEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProducts.BusinessEntities
+{
+    public class CategoryEntity
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public virtual List<ProductEntity> Products { get; set; }
+    }
+}
---
+++ MyProducts.BusinessEntities/ProductEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProducts.BusinessEntities
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual CategoryEntity Category { get; set; }
+
+        public int ManufacturerId { get; set; }
+        public virtual ManufacturerEntity Manufacturer { get; set; }
+
+        public int SupplierId { get; set; }
+        public virtual SupplierEntity Supplier { get; set; }
+    }
+}
