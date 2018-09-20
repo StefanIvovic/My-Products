302b9fbb4764c456e034b38a6e803ee914daf5c3
Stefan Ivovic <stefanivovic91@gmail.com>
2018-09-20 20:25:08 +0200

    added missing _unitOfWork.Save method in CreteSupplier method of SupplierServices

--- MyProducts.BusinessServices/ISupplierServices.cs
+++ MyProducts.BusinessServices/ISupplierServices.cs
@@ -1,6 +1,4 @@
 ﻿using MyProducts.BusinessEntities;
-using MyProducts.DAL;
-using MyProducts.DAL.UnitOfWork;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +10,5 @@ namespace MyProducts.BusinessServices
     public  interface ISupplierServices
     {
         void CreateSupplier(SupplierEntity supplierEntity);
-    }
-
-    public class SupplierServices : ISupplierServices
-    {
-        private UnitOfWork _unitOfWork;
-
-        public SupplierServices(UnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
-        public void CreateSupplier(SupplierEntity supplierEntity)
-        {
-            var supplierDb = AutoMapper.Mapper.Map<SupplierEntity, Supplier>(supplierEntity);
-            _unitOfWork.SupplierRepository.Insert(supplierDb);
-        }
     }
 }
---
+++ MyProducts.BusinessServices/SupplierServices.cs
@@ -0,0 +1,22 @@
+﻿using MyProducts.BusinessEntities;
+using MyProducts.DAL;
+using MyProducts.DAL.UnitOfWork;
+
+namespace MyProducts.BusinessServices
+{
+    public class SupplierServices : ISupplierServices
+    {
+        private UnitOfWork _unitOfWork;
+
+        public SupplierServices(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void CreateSupplier(SupplierEntity supplierEntity)
+        {
+            var supplierDb = AutoMapper.Mapper.Map<SupplierEntity, Supplier>(supplierEntity);
+            _unitOfWork.SupplierRepository.Insert(supplierDb);
+            _unitOfWork.Save();
+        }
+    }
+}
