70e8f3a6bcfdb229aabbd5dc7f02e51181ddf315
Stefan Ivovic <stefanivovic91@gmail.com>
2018-09-20 20:17:12 +0200

    added supplier services with create methd

---
+++ MyProducts.BusinessServices/ISupplierServices.cs
@@ -0,0 +1,31 @@
+﻿using MyProducts.BusinessEntities;
+using MyProducts.DAL;
+using MyProducts.DAL.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProducts.BusinessServices
+{
+    public  interface ISupplierServices
+    {
+        void CreateSupplier(SupplierEntity supplierEntity);
+    }
+
+    public class SupplierServices : ISupplierServices
+    {
+        private UnitOfWork _unitOfWork;
+
+        public SupplierServices(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void CreateSupplier(SupplierEntity supplierEntity)
+        {
+            var supplierDb = AutoMapper.Mapper.Map<SupplierEntity, Supplier>(supplierEntity);
+            _unitOfWork.SupplierRepository.Insert(supplierDb);
+        }
+    }
+}
