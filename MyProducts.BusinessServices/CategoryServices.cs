b0073350c0c780d8f4182b66fdb11c0d595929c2
Stefan Ivovic <stefanivovic91@gmail.com>
2018-09-20 19:37:10 +0200

    working on business services added support for automapper added category service with insert method

---
+++ MyProducts.BusinessServices/CategoryServices.cs
@@ -0,0 +1,24 @@
+﻿using MyProducts.BusinessEntities;
+using MyProducts.DAL;
+using MyProducts.DAL.UnitOfWork;
+
+namespace MyProducts.BusinessServices
+{
+    public class CategoryServices : ICategoryServices
+    {
+        private UnitOfWork _unitOfWork;
+
+        public CategoryServices(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public void CreateCategory(CategoryEntity categoryEntity)
+        {
+            var categoryDb = AutoMapper.Mapper.Map<CategoryEntity, Category>(categoryEntity);
+            _unitOfWork.CategoryRepository.Insert(categoryDb);
+            _unitOfWork.Save();
+        }
+    }
+}
---
+++ MyProducts.BusinessServices/Mapper/MapperConfig.cs
@@ -0,0 +1,32 @@
+﻿using MyProducts.BusinessEntities;
+using MyProducts.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProducts.BusinessServices.Mapper
+{
+    public static class MapperConfig
+    {
+        public static void RegisterMaps()
+        {
+            AutoMapper.Mapper.Initialize(cfg => {
+                //source, destination            
+                cfg.CreateMap<Category, CategoryEntity>();
+                cfg.CreateMap<CategoryEntity, Category>();
+
+                cfg.CreateMap<Manufacturer, ManufacturerEntity>();
+                cfg.CreateMap<ManufacturerEntity, Manufacturer>();
+
+                cfg.CreateMap<Product, ProductEntity>();
+                cfg.CreateMap<ProductEntity, Product>();
+
+                cfg.CreateMap<Supplier, SupplierEntity>();
+                cfg.CreateMap<SupplierEntity, Supplier>();
+
+            });
+        }
+    }
+}
