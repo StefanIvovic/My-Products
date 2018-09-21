9cc1897e70a631b89bd80cc0c0dbcacacd5aeb82
Stefan Ivovic <stefanivovic91@gmail.com>
2018-09-21 10:12:32 +0200

    added dummy data in seed method

--- MyProducts.DAL/Migrations/Configuration.cs
+++ MyProducts.DAL/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace MyProducts.DAL.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,10 +15,50 @@ namespace MyProducts.DAL.Migrations
 
         protected override void Seed(MyProducts.DAL.ProductContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var cat = new List<Category>()
+            {
+                new Category{ Name = "Patike" },
+                new Category{ Name = "Satovi"},
+                new Category{ Name = "Rucni alat"}
+            };
+            var manufac = new List<Manufacturer>()
+            {
+                new Manufacturer{Name="Hilti"},
+                new Manufacturer{Name="Patek Phillipe"},
+                new Manufacturer{Name = "Nike"}
+            };
+            var supplier = new List<Supplier>()
+            {
+                new Supplier{Name = "Djole transport doo"},
+                new Supplier{Name = "EasyTrans INC"}
+            };
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            foreach (var item in cat)
+                context.Categories.AddOrUpdate(c => c.Name, item);
+
+            foreach (var item in manufac)
+                context.Manufacturers.AddOrUpdate(m => m.Name, item);
+
+            foreach (var item in supplier)
+                context.Suppliers.AddOrUpdate(s => s.Name, item);
+
+            context.SaveChanges();
+
+            var products = new List<Product>()
+            {
+                new Product{Name = "AirMax 360",        Description = "patike za trcanje" ,     Price=16000, Category = cat[0], Manufacturer=manufac[2], Supplier=supplier[1] },
+                new Product{Name = "AirMax VapourMax",  Description = "patike za trcanje" ,     Price=18000, Category = cat[0], Manufacturer=manufac[2], Supplier=supplier[0] },
+                new Product{Name = "Nautilus",          Description = "kvalitetan sat" ,        Price=345000, Category = cat[1], Manufacturer=manufac[1], Supplier=supplier[0] },
+                new Product{Name = "grandmaster chime", Description = "Luksuzni sat" ,          Price=550000, Category = cat[1], Manufacturer=manufac[1], Supplier=supplier[0] },
+                new Product{Name = "Power Drill",       Description = "Busilica od 1800W" ,     Price=25000, Category = cat[2], Manufacturer=manufac[0], Supplier=supplier[1] },
+                new Product{Name = "Aku Screwdriver",   Description = "Srafciger na baterije" , Price=22000, Category = cat[2], Manufacturer=manufac[0], Supplier=supplier[1] }
+            };
+
+            foreach (var item in products)
+                context.Products.AddOrUpdate(p => p.Name, item);
+
+            context.SaveChanges();
+
         }
     }
 }
