175dc2ebe979035ded94f60d3beeeff33e86c552
Stefan Ivovic <stefanivovic91@gmail.com>
2018-09-21 10:21:15 +0200

    added required attribute to name property

--- MyProducts.BusinessEntities/SupplierEntity.cs
+++ MyProducts.BusinessEntities/SupplierEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace MyProducts.BusinessEntities
     public class SupplierEntity
     {
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
         public virtual List<ProductEntity> Products { get; set; }
     }
