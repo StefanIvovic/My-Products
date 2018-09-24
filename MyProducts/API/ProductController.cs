c159526a593b0b3a40f89c6b879eb339a4f5347a
Stefan Ivovic <stefanivovic91@gmail.com>
2018-09-24 12:26:14 +0200

    added Web Api for product

---
+++ MyProducts/API/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MyProducts.BusinessEntities;
+using MyProducts.BusinessServices;
+
+namespace MyProducts.API
+{
+    [RoutePrefix("api/product")]
+    public class ProductController : ApiController
+    {
+        private readonly IProductServices _productServices;
+
+
+        public ProductController(IProductServices productServices)
+        {
+            _productServices = productServices;
+        }
+
+        // GET: api/Product/getallproducts
+        [Route("getallproducts")]
+        public HttpResponseMessage GetAllProducts()
+        {
+            var products = _productServices.GetAllProducts();
+            if (products == null || !products.Any())
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Products not found");
+            return Request.CreateResponse(HttpStatusCode.OK, products);
+        }
+
+        // GET: api/Product/GetProductById/5
+        [Route("GetProductById/{id}")]
+        [ResponseType(typeof(ProductEntity))]
+        public HttpResponseMessage GetProductById(int id)
+        {
+            var productEntity = _productServices.GetProductById(id);
+            if (productEntity == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Product not found"); ;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, productEntity);
+        }
+
+        // PUT: api/Product/
+        [Route("UpdateProduct/{productEntity}")]
+        public HttpResponseMessage UpdateProduct(ProductEntity productEntity)
+        {
+            try
+            {
+                _productServices.UpdateProduct(productEntity);
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Something went wrong, product is not updated");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, "Product has been updated");
+        }
+
+        // POST: api/Product
+        [Route("CreateProduct/productEntity")]
+        public HttpResponseMessage CreateProduct(ProductEntity productEntity)
+        {
+            try
+            {
+                _productServices.CreateProduct(productEntity);
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Something went wrong, product is not created");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, "Product has been created");
+        }
+
+        // DELETE: api/Product/5
+        [Route("DeleteProduct/{id}")]
+        public HttpResponseMessage DeleteProduct(int id)
+        {
+            try
+            {
+                _productServices.DeleteProduct(id);
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Something went wrong, product has not been deleted");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Product has been deleted");
+        }
+    }
+}
--- MyProducts/App_Start/WebApiConfig.cs
+++ MyProducts/App_Start/WebApiConfig.cs
@@ -1,7 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MyProducts.BusinessServices;
+using MyProducts.Resolver;
+using MyProducts.Settings;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using Unity;
+using Unity.Lifetime;
 
 namespace MyProducts
 {
@@ -10,6 +15,16 @@ namespace MyProducts
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new StringEnumConverter());
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+
+            //web api dependency resolver configuration
+            var container = new UnityContainer();
+            container.RegisterType<IProductServices, ProductServices>(new HierarchicalLifetimeManager());
+            config.DependencyResolver = new UnityResolver(container);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
